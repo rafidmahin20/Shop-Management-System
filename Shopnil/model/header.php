b1469bc5cc9570f7ca37ac0650cb870a50ff270b
Rafid Mahin <82217716+rafidmahin20@users.noreply.github.com>
2021-08-17 01:59:44 +0600

    Add files via upload

---
+++ Shopnil/model/header.php
@@ -0,0 +1,50 @@
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	
+
+	<link type = "text/css" rel="stylesheet" href="header.css">
+</head>
+<body>
+
+<div style = "background-color : #EFF0EF; height: 50px; width : 100%; ">
+<header>
+<nav>
+<ul style="list-style-type: none; margin: 0; padding: 0;">
+	<li style="float: right; display:inline-block; color : red;"><a style=" background-color: #4CAF50;
+  border: none;
+  color: white;
+  padding: 10px 25px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin:3px 4px;
+  margin-top:5px;
+  border-radius: 4px;
+  cursor: pointer;" href="logout.php" >Logout</a></li>
+
+    <li style="float: right;display:inline-block;"><a href="addproduct.php" style="background-color: #4CAF50;
+  border: none;
+  color: white;
+  padding: 10px 25px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  border-radius: 4px;
+  margin-top:10px;
+  font-size: 16px;
+  margin: 4px 2px;
+  cursor: pointer;" >Add Product</a></li>
+
+</ul>
+</nav>
+</header>
+</div>
+</body>
+</html>
