84041b74b63e58426a0a4be929939e6c90e6acb9
Rafid Mahin <82217716+rafidmahin20@users.noreply.github.com>
2021-08-17 10:22:51 +0600

    Add files via upload

---
+++ Tonwi/control/cusWithdrawVal.php
@@ -0,0 +1,56 @@
+<?php
+$validate=""; 
+$withdrawConfirm="";
+$withdrawError="";
+$userSearch="";
+
+include ("../db/cusDb.php");
+
+if($_SERVER["REQUEST_METHOD"]=="POST")
+{ 
+    $username  = $_SESSION ["username"];
+    $amount=$_REQUEST["amount"];
+    $password = $_REQUEST ["password"];
+    
+    $connection = new db();
+    $conobj=$connection->OpenCon();
+
+    $userQuery=$connection->CheckUserForDepWithLogin($conobj,"customer", $_SESSION['username']);
+
+    if ($userQuery->num_rows > 0) {
+
+        // output data of each row
+        while($row = $userQuery->fetch_assoc()) {
+        
+        $cusPassword = $row["password"];
+        $balanceCustomer = $row["balance"];
+        } 
+     }
+    
+     if($amount=="" || $password==""){
+            $validateWithdraw= "Invalid Password or Amount";
+         }
+
+        else{
+            if($cusPassword==$password){
+
+            $doubleAmount = floatval($amount);
+            $doubleBalance= floatval($balanceCustomer);
+            $updateBalanceCustomer=$doubleBalance-$doubleAmount;
+            if($updateBalanceCustomer<0){
+                $withdrawError = "Not enough balance";
+                $updateBalanceCustomer = $doubleBalance;
+            }
+            else{
+                $userQuery=$connection->UpdateBalanceCustomer($conobj,"customer", $username, $updateBalanceCustomer);
+                $withdrawConfirm ="Withdraw Successful !";
+            }
+            
+             }
+            else{
+                $userSearch="User Not Found";
+            }
+        }   
+    }
+    
+?>
